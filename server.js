fed7d1c412829601514dd77d15ab230f52aacc77
exconf <sergey.sosedkin@gmail.com>
2016-09-26 15:59:14 -0700

    first working app

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 var express = require('express')
-var app = express();
+var strftime = require('strftime')
+var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/:query', function (req, res) {
+  var query = req.params.query
+  var output = {}
+  var time;
+  if (isNaN(Number(query))) { //if not timestamp
+    if (new Date(query) !== 'Invalid Date') { //if valid date
+      time = new Date(query)
+    } else {
+      time = null
+    }
+  } else { //if timestamp
+    console.log(Number(query))
+    time = new Date(Number(query) * 1000)
+  }
+
+  if (time === null) {
+    output = { 'unix': null, 'natural': null }
+  } else {
+    output = { 'unix': time.getTime() / 1000, 'natural': strftime('%B %d, %Y', time) }
+  }
+  console.log(output)
+  res.send(JSON.stringify(output))
+})
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+
+})
